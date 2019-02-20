b3dce3eaf8ce64afe82194b22f4bb26cc13ca8d9
Lakshmikanth V <lakshmikanthvasudev@gmail.com>
2019-02-20 20:21:53 +0530

    Update album.js

--- src/store/reducers/album.js
+++ src/store/reducers/album.js
@@ -1,11 +1,24 @@
 import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../../shared/utility';
 
+/**
+* @desc reducer for Album
+* @version
+* @author Lakshmikanth 
+* @return updated state
+*/
+
+/**
+* @desc initial state of reducer
+*/
 const initialState = {
   album: [],
   photos: []
 };
 
+/**
+* @desc function will manipulate the reducer state with the action payload data.
+*/
 const setAlbum = ( state = initialState , action ) => {
   const album = [...action.album];
   return updateObject( state, { album : album } );   
@@ -16,6 +29,9 @@ const setPhotos = ( state = initialState , action ) => {
   return updateObject( state, { photos: photos } );   
 };
 
+/**
+* @desc checks the action type and calls the respective function and pass state and action to the function.
+*/
 const reducer = ( state = initialState, action ) => {
   switch ( action.type ) {
     case actionTypes.SET_ALBUM: 
