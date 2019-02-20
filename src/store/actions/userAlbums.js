72943325d84853f1fa0f6438db267acc9b859846
Lakshmikanth V <lakshmikanthvasudev@gmail.com>
2019-02-20 20:14:33 +0530

    Update userAlbums.js

--- src/store/actions/userAlbums.js
+++ src/store/actions/userAlbums.js
@@ -6,6 +6,12 @@ import * as actionTypes from './actionTypes';
 * @author  Lakshmikanth
 * @return action and parameters
 */
+
+/**
+* @desc setAlbum dispatch action from container will call this function
+* @argumets  album
+* @return action (will have action type and payload data)
+*/
 export const setAlbum = ( album ) => {
     return {
         type: actionTypes.SET_ALBUM,
